05e4afaed10d2a027b0c40ae9d8671e3dab50d53
Krish-Das <krishdasatgames@gmail.com>
2025-11-23 20:44:29 +0530

    fix(writings): disable search ui

--- apps/writings/app/provider/index.tsx
+++ apps/writings/app/provider/index.tsx
@@ -3,5 +3,5 @@
 import { RootProvider } from "fumadocs-ui/provider/next"
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return <RootProvider>{children}</RootProvider>
+  return <RootProvider search={{ enabled: false }}>{children}</RootProvider>
 }
