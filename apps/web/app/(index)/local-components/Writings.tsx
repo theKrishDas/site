bdc9ae99f6cfe6b250938d8197934fe1ad42a0a1
Krish-Das <krishdasatgames@gmail.com>
2025-11-25 22:10:20 +0530

    chore: refactor rudundant comment

--- apps/web/app/(index)/local-components/Writings.tsx
+++ apps/web/app/(index)/local-components/Writings.tsx
@@ -5,13 +5,6 @@ import { Heading } from "@/components/section-heading"
 import { source } from "@/lib/source"
 
 export default function Writings() {
-  // biome-ignore format: preserve array layout
-  // const writings = [
-  //   { url: "/", data: { title: "Don't pick a niche", description: "My experience building a drawer component for React.", publishDate: "10-02-2025" } },
-  //   { url: "/", data: { title: "Why sharing ideas is important?", description: "What it takes to craft great animations.", publishDate: "11-21-2025" } },
-  //   { url: "/", data: { title: "Convex + Clerk setup", description: "On the importance of sharing your work.", publishDate: "12-31-2025" } },
-  // ]
-
   const publishDate = new Date()
   const writings = source.getPages()
 
