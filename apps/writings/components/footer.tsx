eee72b86c6c44ab1a5affd3883d930e333d00a77
Krish-Das <krishdasatgames@gmail.com>
2025-11-25 18:23:42 +0530

    fix(writings/footer): improve footer layout and styling

--- apps/writings/components/footer.tsx
+++ apps/writings/components/footer.tsx
@@ -10,12 +10,23 @@ export function Footer() {
   return (
     <Container asChild>
       <footer className="mt-20 mb-6 flex w-full items-center justify-between">
-        <div className="pointer-events-none flex select-none items-center gap-1 font-jetbrains-mono font-normal text-label-tertiary text-sm tracking-tight">
-          <span className="copyright font-sfpro">&copy;</span>
-          <span>{footerInfo.copyright}</span>
+        <div className="pointer-events-none flex select-none items-center gap-2.5 font-jetbrains-mono font-normal text-label-tertiary text-sm tracking-tight">
+          <span>
+            <span className="copyright mr-1 font-sfpro">&copy;</span>
+            {footerInfo.copyright}
+          </span>
           <span className="separator">/</span>
-          <span>{footerInfo.location}</span>
+          <span className="before:mr-1.5 before:font-sfpro before:content-['􀙊']">
+            {footerInfo.location}
+          </span>
         </div>
+
+        {/* <div className=""> */}
+        {/*   <span className="select-none font-jetbrains-mono font-normal text-label-tertiary text-sm uppercase tracking-tight before:mr-1.5 before:font-sfpro before:content-['􀆮']"> */}
+        {/*     System */}
+        {/*   </span> */}
+        {/* </div> */}
+
         <ThemeToggle />
       </footer>
     </Container>
