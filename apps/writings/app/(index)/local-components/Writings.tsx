2bb06639c66ba5b61a21af5bf81a9c1913e423b7
Krish-Das <krishdasatgames@gmail.com>
2025-11-25 17:29:14 +0530

    refactor(writings): restructure page layout and components
    Updated the layout of the page to include a footer and a separate writings component, improving organization and readability.

---
+++ apps/writings/app/(index)/local-components/Writings.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@repo/utils"
+import { format } from "date-fns"
+import Link from "next/link"
+import { Heading } from "@/components/section-heading"
+import { source } from "@/lib/source"
+
+export default function Writings() {
+  // biome-ignore format: preserve array layout
+  // const writings = [
+  //   { url: "/", data: { title: "Don't pick a niche", description: "My experience building a drawer component for React.", publishDate: "10-02-2025" } },
+  //   { url: "/", data: { title: "Why sharing ideas is important?", description: "What it takes to craft great animations.", publishDate: "11-21-2025" } },
+  //   { url: "/", data: { title: "Convex + Clerk setup", description: "On the importance of sharing your work.", publishDate: "12-31-2025" } },
+  // ]
+
+  const publishDate = new Date()
+  const writings = source.getPages()
+
+  return (
+    <>
+      <Heading className="mb-4">Writing</Heading>
+      <ul>
+        {writings.map((w) => (
+          <li className="h-fit w-full" key={w.url}>
+            <Link
+              className={cn(
+                "group/writing-link select-nono prose flex cursor-default items-center gap-4",
+                "h-12 after:h-12",
+                "after:-z-1 after:absolute after:inset-x-0 after:bg-fill-quaternary/70 after:opacity-0 after:content-[''] hover:after:opacity-100"
+              )}
+              href={w.url}
+            >
+              <span className="block flex-1 truncate group-hover/writing-link:text-ios-blue">
+                {w.data.title}
+              </span>
+              <span className="font-jetbrains-mono font-normal text-label-tertiary text-sm uppercase tracking-tight">
+                {format(publishDate, "MMM ''dd")}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
--- apps/writings/app/(index)/page.tsx
+++ apps/writings/app/(index)/page.tsx
@@ -1,39 +1,48 @@
-import { ThemeToggle } from "fumadocs-ui/components/layout/theme-toggle"
 import { Container } from "@/components/container"
+import { Footer } from "@/components/footer"
 import { Spacer } from "@/components/spacer"
 import { Header } from "@/components/upsher-header"
 import { SocialIcon } from "./local-components/Socials"
+import Writings from "./local-components/Writings"
 
 export default function Page() {
   return (
     <>
       <Header />
 
-      <Container>
-        <main className="prose">
-          <p>
-            I work on the Web team at Linear. I like to build things for
-            designers and developers, think deeply about the user interface, how
-            it looks, feels, behaves.
-          </p>
-          <p>Previously, I worked on the design team at Vercel.</p>
-          <p>
-            In the past 2 years, I've worked at various startups products and
-            large enterprise level companies. Here's a brief overview.
-          </p>
-          <p className="[&>.socialicon]:mx-0.5">
-            You can see more of my work on{" "}
-            <SocialIcon platform="twitter" url="https://x.com/theupsher" />,
-            view my code on{" "}
-            <SocialIcon platform="github" url="https://github.com/krish-das" />,
-            and reach me via{" "}
-            <SocialIcon platform="email" url="mailto:thekrishdas@gmail.com" />.
-          </p>
-        </main>
+      <Container asChild>
+        <main>
+          <section className="prose">
+            <p>
+              I work on the Web team at Linear. I like to build things for
+              designers and developers, think deeply about the user interface,
+              how it looks, feels, behaves.
+            </p>
+            <p>Previously, I worked on the design team at Vercel.</p>
+            <p>
+              In the past 2 years, I've worked at various startups products and
+              large enterprise level companies. Here's a brief overview.
+            </p>
+            <p className="[&>.socialicon]:mx-0.5">
+              You can see more of my work on{" "}
+              <SocialIcon platform="twitter" url="https://x.com/theupsher" />,
+              view my code on{" "}
+              <SocialIcon
+                platform="github"
+                url="https://github.com/krish-das"
+              />
+              , and reach me via{" "}
+              <SocialIcon platform="email" url="mailto:thekrishdas@gmail.com" />
+              .
+            </p>
+          </section>
 
-        <Spacer className="h-20" />
-        <ThemeToggle />
+          <Spacer className="h-1.5" />
+          <Writings />
+        </main>
       </Container>
+
+      <Footer />
     </>
   )
 }
