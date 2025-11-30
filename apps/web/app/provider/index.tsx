d316eb16148bd80cd6b5a0d5a8b6cfb3208e5dcc
Upsher <158026496+theKrishDas@users.noreply.github.com>
2025-11-30 22:11:23 +0530

    Update environment variables for convex client

--- apps/web/app/provider/index.tsx
+++ apps/web/app/provider/index.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react"
 import { RootProvider } from "fumadocs-ui/provider/next"
 
-const convex = new ConvexReactClient(process.env.CONVEX_DEPLOY_KEY || "")
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
