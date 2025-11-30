16980481c73b29c1594c50de9054ec8a9eb7bf85
Krish-Das <krishdasatgames@gmail.com>
2025-11-30 23:37:04 +0530

    fix(web): fix lint issue

--- apps/web/app/provider/index.tsx
+++ apps/web/app/provider/index.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react"
 import { RootProvider } from "fumadocs-ui/provider/next"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
