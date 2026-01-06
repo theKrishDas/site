f1cb083a75fb4315dc68cebbcccd546faaba512b
Krish-Das <krishdasatgames@gmail.com>
2026-01-06 12:20:00 +0530

    refactor(ui): add Kbd component to MDX for markdown usage
    - Include Kbd component in MDX components for use in markdown files
    - Note: Temporarily adding `not-prose` class to avoid prose styling conflicts

--- apps/web/components/kbd/kbd.tsx
+++ apps/web/components/kbd/kbd.tsx
@@ -2,7 +2,8 @@ import "./kbd.css"
 import { cn } from "@repo/utils"
 
 function Kbd({ className, ...props }: React.ComponentProps<"kbd">) {
-  return <kbd className={cn("Kbd", className)} {...props} />
+  // TODO: Remove this `not-prose` className later when components are finalized
+  return <kbd className={cn("Kbd not-prose", className)} {...props} />
 }
 
 export { Kbd }
