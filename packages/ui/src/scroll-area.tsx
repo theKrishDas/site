e96700bbb50d8f4c048fd66b4bd61e33899489b4
Krish-Das <krishdasatgames@gmail.com>
2026-01-13 15:01:08 +0530

    feat(ui): add scroll-area component

---
+++ packages/ui/src/scroll-area.tsx
@@ -0,0 +1,29 @@
+import { ScrollArea as BUIScrollArea } from "@base-ui/react/scroll-area"
+
+function Viewport({ className, ...props }: BUIScrollArea.Viewport.Props) {
+  return (
+    <BUIScrollArea.Viewport
+      className={`corner-squircle ui:-outline-offset-1 ui:h-full ui:overscroll-contain ui:rounded-2xl focus-visible:outline-ios-blue ui:focus-visible:outline-2 ${className}`}
+      {...props}
+    />
+  )
+}
+
+function Scrollbar({ className, ...props }: BUIScrollArea.Scrollbar.Props) {
+  return (
+    <BUIScrollArea.Scrollbar
+      className={`ui:data-[orientation=vertical]:before:-translate-x-1/2 ui:data-[orientation=horizontal]:before:-bottom-2 ui:pointer-events-none ui:relative ui:flex ui:rounded ui:bg-fill-tertiary ui:opacity-0 ui:transition-opacity ui:before:absolute ui:before:content-[''] ui:data-hovering:pointer-events-auto ui:data-scrolling:pointer-events-auto ui:data-[orientation=horizontal]:m-2 ui:data-[orientation=vertical]:m-2 ui:data-[orientation=horizontal]:h-1 ui:data-[orientation=vertical]:w-1 ui:data-hovering:opacity-100 ui:data-scrolling:opacity-100 ui:data-hovering:delay-0 ui:data-scrolling:duration-0 ui:data-[orientation=horizontal]:before:right-0 ui:data-[orientation=horizontal]:before:left-0 ui:data-[orientation=vertical]:before:left-1/2 ui:data-[orientation=horizontal]:before:h-5 ui:data-[orientation=vertical]:before:h-full ui:data-[orientation=horizontal]:before:w-full ui:data-[orientation=vertical]:before:w-5 ${className}`}
+      {...props}
+    >
+      <BUIScrollArea.Thumb className="ScrollAreaThumb ui:w-full ui:rounded ui:bg-gray-1" />
+    </BUIScrollArea.Scrollbar>
+  )
+}
+
+export const ScrollArea = {
+  Root: BUIScrollArea.Root,
+  Scrollbar,
+  Viewport,
+  Content: BUIScrollArea.Content,
+  Corner: BUIScrollArea.Corner,
+}
